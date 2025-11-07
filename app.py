f6bf1810d8976fb739c812b8c41c96e26ce4e39b
Tim <dev.atobatele@gmail.com>
2025-11-07 13:15:51 -0500

    added watermark to video, progress bar, and fps limits for faster processing

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
 import streamlit as st
-from moviepy import VideoFileClip, AudioFileClip, concatenate_videoclips, ImageClip
+from moviepy import CompositeVideoClip, TextClip, VideoFileClip, AudioFileClip, concatenate_videoclips, ImageClip
 import tempfile
 import os
 
 st.title("MP3 to MP4")
 
 # User uploads
-video_file = st.file_uploader("Upload a video (.mp4), gif, or image", type=["mp4", "gif", "png", "jpg", "jpeg"])
-audio_file = st.file_uploader("Upload an audio track (.mp3)", type=["mp3", "wav", "ogg"])
+video_file = st.file_uploader("Upload a video, gif, or image. It will be looped and trimmed to the audio duration.", type=["mp4", "gif", "png", "jpg", "jpeg","mkv", "mov", "avi", "webm"])
+audio_file = st.file_uploader("Upload an audio track", type=["mp3", "wav", "ogg", "flac","m4a","aac","aiff"])
+progress_bar = st.progress(0)
+
+txt_clip = TextClip(
+    text="mp3-to-mp4.streamlit.app",
+    font_size=20,
+    margin=(30, 30),
+    color='white'
+)
 
 if video_file and audio_file:
+    progress_bar.progress(10, "Starting processing...")
     # Save the uploaded files to a temporary directory
     with tempfile.TemporaryDirectory() as tmpdir:
         video_path = os.path.join(tmpdir, video_file.name)
@@ -28,10 +37,15 @@ if video_file and audio_file:
             video_clip = (ImageClip(video_path)
                         .with_duration(audio_clip.duration)
                         .with_audio(audio_clip))
+            clip_w_url = CompositeVideoClip([video_clip, txt_clip.with_duration(video_clip.duration)])
             output_path = os.path.join(tmpdir, "output.mp4")
-            video_clip.write_videofile(output_path, codec="libx264", audio_codec="aac")
+            progress_bar.progress(70, "Rendering video...")
+            clip_w_url.write_videofile(output_path, fps=1 , codec="libx264", audio_codec="aac", audio_bitrate="320k")
             video_clip.close()
             audio_clip.close()
+            txt_clip.close()
+            clip_w_url.close()
+            progress_bar.progress(100)
         else:
             # Process as video/gif
             video_clip = VideoFileClip(video_path)
@@ -40,12 +54,17 @@ if video_file and audio_file:
             video_loops = [video_clip.copy() for _ in range(num_loops)]
             looped_video = concatenate_videoclips(video_loops)
             final_clip = looped_video.subclipped(0, audio_clip.duration).with_audio(audio_clip)
+            clip_w_url = CompositeVideoClip([final_clip, txt_clip.with_duration(final_clip.duration)])
             output_path = os.path.join(tmpdir, "output.mp4")
-            final_clip.write_videofile(output_path, codec="libx264", audio_codec="aac")
+            progress_bar.progress(70, "Rendering video...")
+            clip_w_url.write_videofile(output_path, fps=24, codec="libx264", audio_codec="aac", audio_bitrate="320k")
             video_clip.close()
             audio_clip.close()
             final_clip.close()
-        
+            looped_video.close()
+            txt_clip.close()
+            clip_w_url.close()
+            progress_bar.progress(100)
         # Present for download
         with open(output_path, "rb") as f:
             st.success("Video created! Download below.")
